5505528b501bf4583f729d32148365450ad6b460
Paul Williams <paul@paulswilliams.me.uk>
2014-11-03 20:37:30 +0000

    first test compiling

--- lib/application_runner.rb
+++ lib/application_runner.rb
@@ -1,2 +1,13 @@
 class ApplicationRunner
+  def start_bidding_in(auction)
+  end
+
+  def announce_closed
+  end
+
+  def shows_sniper_has_lost_auction
+  end
+
+  def stop
+  end
 end
--- lib/fake_action_server.rb
+++ lib/fake_action_server.rb
@@ -1,4 +1,13 @@
 class FakeAuctionServer 
   def initialize item_id
   end
+
+  def start_selling_item
+  end
+
+  def has_received_join_request_from_snipper 
+  end
+
+  def stop
+  end
 end
